89e0b2acc7e8fed0ef024f34e548389870de98e0
Jitendra Kumar <jitendrayadav7078@gmail.com>
2024-07-09 00:33:03 +0530

    This is user service api

---
+++ src/main/java/com/jitendra/service/UserService.java
@@ -0,0 +1,20 @@
+package com.jitendra.service;
+
+import com.jitendra.dto.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    public List<User> getAllUser() {
+        List<User> newUser = new ArrayList<>();
+        newUser.add(new User(101, "Jitendra"));
+        newUser.add(new User(100, "Yadav"));
+        newUser.add(new User(103, "Pnkajm"));
+        return newUser;
+    }
+
+}
---
+++ src/test/java/com/jitendra/UserServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jitendra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
