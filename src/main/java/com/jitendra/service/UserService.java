319b4ce3f78ee0f937e2ba7f9c8b56426b6833ea
Jitendra Kumar <jitendrayadav7078@gmail.com>
2024-07-09 00:53:14 +0530

    Add one more use in this app

--- src/main/java/com/jitendra/service/UserService.java
+++ src/main/java/com/jitendra/service/UserService.java
@@ -13,7 +13,8 @@ public class UserService {
         List<User> newUser = new ArrayList<>();
         newUser.add(new User(101, "Jitendra"));
         newUser.add(new User(100, "Yadav"));
-        newUser.add(new User(103, "Pnkajm"));
+        newUser.add(new User(103, "Pankaj");
+        newUser.add(new User(104, "Shtam"));
         return newUser;
     }
 
