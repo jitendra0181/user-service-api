32f1cc2cad77763917b82373987035236a482437
Jitendra Kumar <jitendrayadav7078@gmail.com>
2024-07-09 01:03:07 +0530

    This is one compile time error that is )

--- src/main/java/com/jitendra/service/UserService.java
+++ src/main/java/com/jitendra/service/UserService.java
@@ -13,7 +13,7 @@ public class UserService {
         List<User> newUser = new ArrayList<>();
         newUser.add(new User(101, "Jitendra"));
         newUser.add(new User(100, "Yadav"));
-        newUser.add(new User(103, "Pankaj");
+        newUser.add(new User(103, "Pankaj"));
         newUser.add(new User(104, "Shtam"));
         return newUser;
     }
